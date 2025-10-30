0c598202d3cfff63e639f9aa10be1fb6cd2f80a3
haoworld886 <howorld0323@gmail.com>
2025-10-30 11:40:10 +0800

    Initial Commit

---
+++ BannerCarouselTestTests/BannerCarouselTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  BannerCarouselTestTests.swift
+//  BannerCarouselTestTests
+//
+//  Created by howard_lee on 2025/10/30.
+//
+
+import Testing
+@testable import BannerCarouselTest
+
+struct BannerCarouselTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
